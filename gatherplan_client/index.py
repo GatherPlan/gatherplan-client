cf27e359d62d396363e3818733beb56fe808375e
wjdqlsdlsp <wjdqlsdlsp@naver.com>
2024-03-20 00:29:33 +0900

    sign_up page

--- gatherplan_client/index.py
+++ gatherplan_client/index.py
@@ -6,47 +6,4 @@ class State(rx.State):
 
 
 def index() -> rx.Component:
-    return rx.center(
-        rx.vstack(
-            rx.heading("언제볼까", size="6"),
-            rx.box(
-                height="30%",
-            ),
-            rx.button(
-                "약속 만들기",
-                size="4",
-                width="80%",
-                color="white",
-                bg_color="black",
-                border_radius="5px",
-                on_click=rx.redirect("/sign_up"),
-            ),
-            rx.button(
-                "약속 참여하기",
-                size="4",
-                width="80%",
-                color="white",
-                bg_color="black",
-                border_radius="5px",
-                on_click=rx.redirect(
-                    "https://github.com/reflex-dev/reflex/",
-                    external=True,
-                ),
-            ),
-            rx.button(
-                "약속 현황보기",
-                size="4",
-                width="80%",
-                color="white",
-                bg_color="black",
-                border_radius="5px",
-                on_click=rx.console_log("Hello World!"),
-            ),
-            width="70%",
-            height="80%",
-            font_size="2em",
-            spacing="5",
-            align="center",
-        ),
-        height="100vh",
-    )
+    return rx.center()
