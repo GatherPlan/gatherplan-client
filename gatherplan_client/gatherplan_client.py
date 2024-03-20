1175a8a33ea39e6d92753faac7830c99eef60c1e
Park JeongBin <61408680+wjdqlsdlsp@users.noreply.github.com>
2024-03-20 23:55:24 +0900

    Feature/sign up (#4)
    * sign_up page
    
    * \n 추가
    
    * make_meeting 추가

--- gatherplan_client/gatherplan_client.py
+++ gatherplan_client/gatherplan_client.py
@@ -1,8 +1,10 @@
 import reflex as rx
 from .index import index
 from .sign_up import sign_up
+from .make_meeting.make_meeting import make_meeting
 
 
 app = rx.App(stylesheets=["fonts/myfont.css"], title="GatherPlan")
 app.add_page(index)
 app.add_page(sign_up, route="/sign_up")
+app.add_page(make_meeting, route="/make_meeting")
---
+++ gatherplan_client/make_meeting/make_meeting.py
@@ -0,0 +1,5 @@
+import reflex as rx
+
+
+def make_meeting() -> rx.Component:
+    return rx.vstack()
