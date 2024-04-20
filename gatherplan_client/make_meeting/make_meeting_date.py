dc7f65662da05b10bde14a5797e82a60b498a96e
ppippi-dev <61408680+ppippi-dev@users.noreply.github.com>
2024-04-21 01:36:38 +0900

    datetime이용하도록 직접 만들어야할듯 (#9)
    * datetime이용하도록 직접 만들어야할듯
    
    * 지금까지 한거라도...
    
    * color 입힘
    
    ---------
    
    Co-authored-by: ppippi-dev <ppippi-dev@naver.com>

--- gatherplan_client/make_meeting/make_meeting_date.py
+++ gatherplan_client/make_meeting/make_meeting_date.py
@@ -1,28 +1,202 @@
+from typing import List, Dict
+
 import reflex as rx
+import datetime
+import calendar
+from dateutil.relativedelta import relativedelta
+
 
 from gatherplan_client.login import need_login
-from gatherplan_client.make_meeting.make_meeting import MakeMeetingNameState
 from gatherplan_client.reflex_assets.buttons import basic_button
-from gatherplan_client.reflex_assets.form_box import form_box, form_box_with_value
 from gatherplan_client.reflex_assets.header import header
-from gatherplan_client.reflex_assets.schema import TextSize
+from gatherplan_client.reflex_assets.schema import TextSize, AppColor, AppFontFamily
 from gatherplan_client.reflex_assets.text_box import left_align_text_box
 
 
-def location_button(button_text: str):
-    return rx.drawer.close(
-        rx.button(
-            button_text,
-            width="340px",
-            height="36px",
-            color="#A3A3A3",
-            type="button",
-            on_click=MakeMeetingNameState.handle_location_submit(
-                {"input_location": button_text}
+def location_button(display_data: List):
+
+    return rx.cond(
+        CalendarSelect.holiday_data[display_data[0]] == "sun",
+        rx.cond(
+            CalendarSelect.holiday_data[display_data[0]] == "prev",
+            rx.button(
+                display_data[0].to_string(json=False).split("-")[2],
+                width="50px",
+                height="36px",
+                color=AppColor.RED,
+                font_size="16px",
+                type="button",
+                background_color=AppColor.BACKGROUND_GRAY_COLOR,
+                disabled=True,
+                on_click=CalendarSelect.click_button(display_data[0]),
             ),
-            background_color="#FFFFFF",
-        )
+            rx.cond(
+                display_data[1],
+                rx.button(
+                    display_data[0].to_string(json=False).split("-")[2],
+                    width="50px",
+                    height="36px",
+                    color=AppColor.RED,
+                    font_size="16px",
+                    type="button",
+                    background_color=AppColor.BACKGROUND_GRAY_COLOR,
+                    on_click=CalendarSelect.click_button(display_data[0]),
+                ),
+                rx.button(
+                    display_data[0].to_string(json=False).split("-")[2],
+                    width="50px",
+                    height="36px",
+                    color=AppColor.RED,
+                    font_size="16px",
+                    type="button",
+                    background_color=AppColor.WHITE,
+                    on_click=CalendarSelect.click_button(display_data[0]),
+                ),
+            ),
+        ),
+        rx.cond(
+            CalendarSelect.holiday_data[display_data[0]] == "sat",
+            rx.cond(
+                CalendarSelect.holiday_data[display_data[0]] == "prev",
+                rx.button(
+                    display_data[0].to_string(json=False).split("-")[2],
+                    width="50px",
+                    height="36px",
+                    color=AppColor.BLUE,
+                    font_size="16px",
+                    type="button",
+                    background_color=AppColor.BACKGROUND_GRAY_COLOR,
+                    disabled=True,
+                    on_click=CalendarSelect.click_button(display_data[0]),
+                ),
+                rx.cond(
+                    display_data[1],
+                    rx.button(
+                        display_data[0].to_string(json=False).split("-")[2],
+                        width="50px",
+                        height="36px",
+                        color=AppColor.BLUE,
+                        font_size="16px",
+                        type="button",
+                        background_color=AppColor.BACKGROUND_GRAY_COLOR,
+                        on_click=CalendarSelect.click_button(display_data[0]),
+                    ),
+                    rx.button(
+                        display_data[0].to_string(json=False).split("-")[2],
+                        width="50px",
+                        height="36px",
+                        color=AppColor.BLUE,
+                        font_size="16px",
+                        type="button",
+                        background_color=AppColor.WHITE,
+                        on_click=CalendarSelect.click_button(display_data[0]),
+                    ),
+                ),
+            ),
+            rx.cond(
+                CalendarSelect.holiday_data[display_data[0]] == "prev",
+                rx.button(
+                    display_data[0].to_string(json=False).split("-")[2],
+                    width="50px",
+                    height="36px",
+                    color="#000000",
+                    font_size="16px",
+                    type="button",
+                    background_color=AppColor.BACKGROUND_GRAY_COLOR,
+                    disabled=True,
+                    on_click=CalendarSelect.click_button(display_data[0]),
+                ),
+                rx.cond(
+                    display_data[1],
+                    rx.button(
+                        display_data[0].to_string(json=False).split("-")[2],
+                        width="50px",
+                        height="36px",
+                        color="#000000",
+                        font_size="16px",
+                        type="button",
+                        background_color=AppColor.BACKGROUND_GRAY_COLOR,
+                        on_click=CalendarSelect.click_button(display_data[0]),
+                    ),
+                    rx.button(
+                        display_data[0].to_string(json=False).split("-")[2],
+                        width="50px",
+                        height="36px",
+                        color="#000000",
+                        font_size="16px",
+                        type="button",
+                        background_color=AppColor.WHITE,
+                        on_click=CalendarSelect.click_button(display_data[0]),
+                    ),
+                ),
+            ),
+        ),
     )
+
+
+class CalendarSelect(rx.State):
+    display_data: Dict[str, bool] = {}
+    holiday_data: Dict[str, str] = {}
+    select_data: List[str] = []
+
+    setting_time = datetime.datetime.now()
+    setting_time_display = setting_time.strftime("%Y-%m")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._setting_month_calendar()
+
+    def click_button(self, click_data: List):
+        if self.display_data[click_data]:
+            self.select_data.remove(click_data)
+            self.display_data[click_data] = False
+        else:
+            self.select_data.append(click_data)
+            self.display_data[click_data] = True
+
+    def month_decrement(self):
+        self.setting_time = self.setting_time - relativedelta(months=1)
+        self.setting_time_display = self.setting_time.strftime("%Y-%m")
+        self._setting_month_calendar()
+
+    def month_increment(self):
+        self.setting_time = self.setting_time + relativedelta(months=1)
+        self.setting_time_display = self.setting_time.strftime("%Y-%m")
+        self._setting_month_calendar()
+
+    def _setting_month_calendar(self):
+        self.display_data = {}
+
+        weekday = (
+            datetime.date(self.setting_time.year, self.setting_time.month, 1).weekday()
+            + 1
+        )
+
+        for i in range(weekday):
+            temp = " " * i
+            self.display_data[temp] = False
+            self.holiday_data[temp] = "prev"
+
+        for i in range(
+            1,
+            calendar.monthrange(self.setting_time.year, self.setting_time.month)[1] + 1,
+        ):
+            self.display_data[
+                f"{self.setting_time.year}-{self.setting_time.month}-{i}"
+            ] = False
+
+            weekday = datetime.date(
+                self.setting_time.year, self.setting_time.month, i
+            ).weekday()
+
+            self.holiday_data[
+                f"{self.setting_time.year}-{self.setting_time.month}-{i}"
+            ] = ("sun" if weekday == 6 else "sat" if weekday == 5 else "normal")
+
+        for clicked_data in self.select_data:
+            if clicked_data in self.display_data.keys():
+                self.display_data[clicked_data] = True
 
 
 @need_login
@@ -35,27 +209,43 @@ def make_meeting_date() -> rx.Component:
             main_font_size=TextSize.TINY_SMALL,
             sub_font_size=TextSize.TINY,
         ),
-        # TODO: Implement the date picker
-        rx.vstack(
-            rx.chakra.hstack(
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
+        rx.center(
+            rx.button("<", on_click=CalendarSelect.month_decrement),
+            rx.text(
+                CalendarSelect.setting_time_display,
+                width="100px",
+                height="45px",
+                align="center",
             ),
-            rx.chakra.hstack(
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-                rx.chakra.checkbox("4/16", color_scheme="red", size="sm"),
-            ),
+            rx.button(">", on_click=CalendarSelect.month_increment),
+            color=AppColor.BLACK,
+            font_size=TextSize.MEDIUM,
+            font_family=AppFontFamily.DEFAULT_FONT,
+            height="45px",
+            width="100%",
+            font_weight="600",
+            background_color=AppColor.WHITE,
         ),
+        rx.center(
+            rx.grid(
+                rx.center("일", color=AppColor.RED, width="50px"),
+                rx.center("월", width="50px"),
+                rx.center("화", width="50px"),
+                rx.center("수", width="50px"),
+                rx.center("목", width="50px"),
+                rx.center("금", width="50px"),
+                rx.center("토", color=AppColor.BLUE, width="50px"),
+                rx.foreach(
+                    CalendarSelect.display_data,
+                    location_button,
+                ),
+                columns="7",
+                align="center",
+                width="360px",
+            ),
+            width="100%",
+        ),
+        # TODO: Implement the date picker
         basic_button("다음"),
         spacing="0",
         height="100vh",
