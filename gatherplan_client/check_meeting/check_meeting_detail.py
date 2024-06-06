974184493ac48e7459b5a1e6c9045329709037b1
ppippi <wjdqlsdlsp@naver.com>
2024-06-07 08:12:49 +0900

    fix(check_meeting_detail): ui 변경

--- gatherplan_client/check_meeting/check_meeting_detail.py
+++ gatherplan_client/check_meeting/check_meeting_detail.py
@@ -52,65 +52,26 @@ def check_meeting_detail_not_logined() -> rx.Component:
                 font_weight="700",
                 width="360px",
             ),
-            width="100%",
-            height="15%",
-        ),
-        rx.form(
             rx.center(
-                rx.vstack(
-                    rx.text(
-                        "약속 목록",
-                        font_size="14px",
-                        font_family=AppFontFamily.DEFAULT_FONT,
-                        font_weight="700",
-                        padding_left="10px",
-                        color=AppColor.BLACK,
-                        width="360px",
+                rx.tabs.root(
+                    rx.tabs.list(
+                        rx.tabs.trigger("Tab 1", value="tab1"),
+                        rx.tabs.trigger("Tab 2", value="tab2"),
                     ),
-                    rx.text(
-                        "약속 이름 또는 호스트 이름을 검색해보세요",
-                        font_size="12px",
-                        font_family=AppFontFamily.DEFAULT_FONT,
-                        color=AppColor.GRAY_TEXT,
-                        font_weight="700",
-                        padding_left="10px",
-                        padding_bottom="5px",
-                        width="360px",
+                    rx.tabs.content(
+                        rx.text("item on tab 1"),
+                        value="tab1",
                     ),
-                    align="center",
+                    rx.tabs.content(
+                        rx.text("item on tab 2"),
+                        value="tab2",
+                    ),
                     width="100%",
                 ),
+                width="100%",
             ),
-            rx.center(
-                rx.hstack(
-                    rx.box(
-                        rx.input(
-                            placeholder="홍길동",
-                            name="location",
-                            font_size="10px",
-                            height="35px",
-                            border_radius="35px",
-                            type="text",
-                        ),
-                        padding_bottom="20px",
-                        padding_left="10px",
-                        width="300px",
-                    ),
-                    rx.button(
-                        "검색",
-                        height="35px",
-                        width="50px",
-                        background_color=AppColor.MAIN_COLOR,
-                        type="submit",
-                        font_size="12px",
-                    ),
-                    width="360px",
-                )
-            ),
-            on_submit=CheckState.handle_detail_submit,
             width="100%",
-            align="center",
-            height="70%",
+            height="15%",
         ),
         spacing="0",
         height="100vh",
