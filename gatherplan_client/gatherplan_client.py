0e02c074df39578528f31634c8725445d49f0837
ppippi <wjdqlsdlsp@naver.com>
2024-10-28 00:30:57 +0900

    local에서는 head component를 제거한다

--- gatherplan_client/gatherplan_client.py
+++ gatherplan_client/gatherplan_client.py
@@ -1,27 +1,35 @@
+import os
 from .pages import *  # noqa: F401, F403
 
-app = rx.App(
-    stylesheets=["fonts/myfont.css"],
-    title="GatherPlan",
-    head_components=[
-        rx.el.script(
-            async_=True, src="https://www.googletagmanager.com/gtag/js?id=G-Q9R0ZEJ8S6"
-        ),
-        rx.el.script(
-            """
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+if os.environ.get("ENV") == "prod":
+    app = rx.App(
+        stylesheets=["fonts/myfont.css"],
+        title="GatherPlan",
+        head_components=[
+            rx.el.script(
+                async_=True,
+                src="https://www.googletagmanager.com/gtag/js?id=G-Q9R0ZEJ8S6",
+            ),
+            rx.el.script(
+                """
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-gtag('config', 'G-Q9R0ZEJ8S6');
-            """
-        ),
-        rx.el.meta(
-            name="google-site-verification",
-            content="beqAu5H-vKsKPxSVN6z3hpR6ydUfSKqWjcmzqY-6000",
-        ),
-    ],
-)
+    gtag('config', 'G-Q9R0ZEJ8S6');
+                """
+            ),
+            rx.el.meta(
+                name="google-site-verification",
+                content="beqAu5H-vKsKPxSVN6z3hpR6ydUfSKqWjcmzqY-6000",
+            ),
+        ],
+    )
+else:
+    app = rx.App(
+        stylesheets=["fonts/myfont.css"],
+        title="GatherPlan",
+    )
 
 
 def health_check() -> rx.Component:
